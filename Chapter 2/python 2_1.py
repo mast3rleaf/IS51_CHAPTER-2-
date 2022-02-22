83175395476b9de99b870d2394e6bbe36836ee2e
Lyle Buendia <lylebuendia@MacBook-Pro.local>
2022-02-22 14:20:28 -0800

    initial commit

---
+++ Chapter 2/python 2_1.py
@@ -0,0 +1,45 @@
+
+
+
+
+
+
+
+# print("3/2,3/2)
+
+# speed = 50
+# time_elapsed = 14
+# distance = speed * time_elapsed
+# print(distance)
+
+"""
+this is a multi line comment
+"""
+# This is a one line comment
+
+
+
+# a = 2
+# b = 3
+# print(abs(1-(4*b)))
+
+
+# num = 4
+
+
+# num2 = num
+
+# num3 = 0
+# num3 += num2
+# num3 -= 2
+# print("num3", num3)
+# x = 0
+
+# x = x + 1 # x += 1
+
+
+
+
+inches = 10 // 3
+
+print ("inches", inches)
