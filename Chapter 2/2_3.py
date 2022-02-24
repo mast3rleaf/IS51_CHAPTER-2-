831f35f54a6a951f730a8656139105465009a87f
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-02-24 15:28:15 -0800

    adding new code

---
+++ Chapter 2/2_3.py
@@ -0,0 +1,11 @@
+print("one", "two", "three", sep=",")
+
+print("Hello", end=" ")
+print("World!")
+
+
+print("a\tb\tc")
+print("a\tb\tc\nd\te\tf")
+
+
+print("\"Hello World!\"")                         
