d90247548874fe6a02220cd0de4805c22f1321da
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-02-24 14:55:17 -0800

    2.2

---
+++ Chapter 2/2_2.py
@@ -0,0 +1,54 @@
+
+#myvar = "Say ain't so, Joe"
+#print(myvar)
+
+
+#var2 = "Spam & Eggs"
+
+#spam = "Spam"
+#eggs = "Eggs"
+#spam_and_eggs = spam + " & " + eggs
+
+#print(spam_and_eggs[:]) #UP TO 3 NOT INCLUDING 
+
+#length = len(spam_and_eggs)
+#print(length)
+#print(spam_and_eggs.upper())
+
+
+#praise = "Good Doggie"
+
+#count = praise.upper().count("G")
+#print("Count:",count)
+
+# Lyle Buendia
+# full_name = input("Enter your full name: ")
+# index_of_space = full_name.rfind(" ")
+# first_name = full_name[:index_of_space]
+# last_name = full_name[index_of_space+1:]
+# print("First name:", first_name)
+# print("Last name", last_name)
+
+# annual_income = eval(input("Enter your annual income: "))
+# print("User's income is ", annual_income)
+# monthly_income = annual_income/12
+# monthly_income = eval(annual_income)/12 *OR
+# print("Monthly income: ", monthly_income)
+
+# var1 = 1
+# var2 = 2 
+# var3 = "3"
+# var4 = "4"  
+
+# var5 = var1+var2 # 3 Int
+# var6 = var3+var4 # 34 String
+
+str1 = "Python" 
+
+length = len(str1) #5 Int
+
+str2 = str1[:-1] #Python String
+
+str3 = str1[10] #Error
+
+str4 = str2[2:-1] #tho
