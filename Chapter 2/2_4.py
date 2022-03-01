d815fd9c082c65503878bac2d962658af16c2bdd
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-03-01 15:33:38 -0800

    ?

---
+++ Chapter 2/2_4.py
@@ -0,0 +1,11 @@
+num1 = 1
+num2 = 10 
+num3 = 1000 
+sum_of_all_numbers = num1 + num2 + num3
+
+print("Sum is {0:n}".format(sum_of_all_numbers))
+
+list_of_numbers = (1, 10, 1000) 
+
+print(sum(list_of_numbers))
+
