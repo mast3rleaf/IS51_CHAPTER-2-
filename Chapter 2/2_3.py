bda23d39dc5de97fe4c8d2359c365bd51797ba23
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-03-01 15:30:41 -0800

    2.4

--- Chapter 2/2_3.py
+++ Chapter 2/2_3.py
@@ -13,5 +13,5 @@ print("{0:^5n}{1:<20s}{2:>3n}".format(1, "Barry Bonds", 762))
 print("{0:^5n}{1:<20s}{2:>3n}".format(2, "Hank Aaron", 755))
 print("{0:^5n}{1:<20s}{2:>3n}".format(3, "Babe Ruth", 714))
 
-# str2 = "The population of {0:s} is {1:.2%} of the U.S. population."
-# print(str2.format("Texas", 26440000/309000000))
+str2 = "The population of {0:s} is {1:.2%} of the U.S. population."
+print(str2.format("Texas", 26440000/309000000))
