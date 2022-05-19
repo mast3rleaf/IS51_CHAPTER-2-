a0de6d06ae6413becd40f7c531eea9f73d733b15
Lyle Buendia <lylebuendia@mail.fresnostate.edu>
2022-05-19 03:02:41 -0700

    done

--- Chapter 2/2_4.py
+++ Chapter 2/2_4.py
@@ -1,11 +1,23 @@
-num1 = 1
-num2 = 10 
-num3 = 1000 
-sum_of_all_numbers = num1 + num2 + num3
+# num1 = 1
+# num2 = 10 
+# num3 = 1000 
+# sum_of_all_numbers = num1 + num2 + num3
 
-print("Sum is {0:n}".format(sum_of_all_numbers))
+# print("Sum is {0:n}".format(sum_of_all_numbers))
 
-list_of_numbers = (1, 10, 1000) 
+# list_of_numbers = (1, 10, 1000) 
 
-print(sum(list_of_numbers))
+# print(sum(list_of_numbers))
 
+grades = []
+
+grades.append(1)
+grades.append(2)
+grades.append(4)
+grades.append(9)
+
+print("grades", grades)
+
+length = len(grades) 
+print("length", length)
+print("sliced:", grades[0:2])
